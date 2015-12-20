0e8d618f7b5d2cf9719d9ffda9dae92cfca946c3
Charles Stewart <chuckination@gmail.com>
2015-12-20 01:08:13 -0500

    some comedic reactions to the hello world plugin

--- plugins/hello.rb
+++ plugins/hello.rb
@@ -4,10 +4,25 @@ module TrickBot
   class Hello
     include Cinch::Plugin
 
-    match /hello/
+    match /hello/, method: :on_hello
+    match /kicks (the bot|trickbot)/, use_prefix: false, react_on: :action, method: :on_kick
+    match /p[ea]ts (the bot|trickbot)/, use_prefix: false, react_on: :action, method: :on_pet
 
-    def execute(m)
+    def on_hello(m)
       m.reply "Hello, #{m.user.nick}"
+    end
+
+    def on_kick(m)
+      m.reply "#{m.user.nick}: Ouch!"
+    end
+
+    def on_pet(m)
+      responses = [
+        "m.action_reply 'wags his tail.'",
+        "m.action_reply 'happily pants.'",
+        "m.reply '#{m.user.nick}: Woof!'",
+      ]
+      eval(responses.sample)
     end
   end
 end
