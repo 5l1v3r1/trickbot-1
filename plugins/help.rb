a455226b690763ec8abb2ee4559fb870fcc912e7
Charles Stewart <chuckination@gmail.com>
2016-03-03 00:01:55 -0500

    Use notices for help messages

--- plugins/help.rb
+++ plugins/help.rb
@@ -7,14 +7,14 @@ module TrickBot
     match /help/
 
     def execute(m)
-      m.reply "Hello, #{m.user.nick}. I'm #{m.bot.config.nick}.  I do the following:"
-      m.reply "- respond to people that say hello, e.g. \"trickbot: hello\""
-      m.reply "- resolve titles for URLs posted to channels that point to an HTML document."
-      m.reply "- resolve titles for YouTube URLs posted to channels."
-      m.reply "- search wikipedia for you, e.g. \"trickbot: wiki IRC Bot\""
-      m.reply "- search urban dictionary for you, e.g. \"trickbot: urban urban dictionary\""
-      m.reply "- generate a BOFH excuse, e.g. \"trickbot: bofh\" or \"trickbot: excuse\""
-      m.reply "INSTALLED PLUGINS: #{m.bot.config.plugins.plugins.join(", ")}"
+      m.user.notice "Hello, #{m.user.nick}. I'm #{m.bot.config.nick}.  I do the following:"
+      m.user.notice "- respond to people that say hello, e.g. \"trickbot: hello\""
+      m.user.notice "- resolve titles for URLs posted to channels that point to an HTML document."
+      m.user.notice "- resolve titles for YouTube URLs posted to channels."
+      m.user.notice "- search wikipedia for you, e.g. \"trickbot: wiki IRC Bot\""
+      m.user.notice "- search urban dictionary for you, e.g. \"trickbot: urban urban dictionary\""
+      m.user.notice "- generate a BOFH excuse, e.g. \"trickbot: bofh\" or \"trickbot: excuse\""
+      m.user.notice "INSTALLED PLUGINS: #{m.bot.config.plugins.plugins.join(", ")}"
     end
   end
 end
